ae785f763514f29ebf981716bccf4c257f7c7017
RediiChen <a0929052958@gmail.com>
2021-07-15 23:00:59 +0800

    完成官方的練習

--- src/index.jsx
+++ src/index.jsx
@@ -11,54 +11,20 @@ function Square(props) {
     );
 }
 class Board extends React.Component {
-    // 建構子
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            // 變數名為 squares，使用react的array建構函數
-            // (9)指定9個位子，(?) what if w/o fill null
-            xIsNext: true,
-            // 預設 'X' 玩家先手
-        };
-    }
     // 函數集 methods
-    handleClick(i) {
-        const nine = this.state.squares.slice();
-        if (calculateWinner(nine) || nine[i]) return ;
-        /* 如果已經贏了 OR 點了已經有棋子的地方，拒絕反應。 */
-        nine[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            squares: nine,
-            xIsNext: !this.state.xIsNext,
-        });
-        /* 因為要改的東西有兩個以上，故以物件格式來包裝 */
-    }
     renderSquare(i) {
         return (
             <Square
-                value={this.state.squares[i]}
-                onClick={() => this.handleClick(i)}
+                value={this.props.squares[i]}
+                onClick={() => this.props.onClick(i)}
             />
         );
     }
     // 渲染區
     render() {
-        //* 細部的調整 */
-        const winner = calculateWinner(this.state.squares) ;
-        // 雖然是 const，但是一有改變，就會重新 render。
-        let status ;
-        if (winner) {
-            status = '獲勝者是: ' + winner ;
-        } else if (this.state.squares.indexOf(null) === -1) {
-            status = '平手' ;
-        } else {
-            status = '現在輪到: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
         //* 實際的呈現 */
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -80,15 +46,92 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [
+                {
+                    squares: Array(9).fill(null),
+                }
+            ],
+            stepNum: 0,
+            xIsNext: true,
+        };
+    }
+    handleClick(i) {
+        const history = this.state.history.slice(0, this.state.stepNum + 1);
+        //-> 物件的陣列
+        const current = history[history.length - 1];//-> 物件
+        const nine = current.squares.slice();       //-> 物件中的陣列
+
+        if (calculateWinner(nine) || nine[i]) return;
+        /* 如果已經贏了 OR 點了已經有棋子的地方，拒絕反應。 */
+
+        nine[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            history: history.concat(
+                [{
+                    squares: nine,
+                }]
+            ),
+            stepNum: history.length,
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+    jumpTo(step) {
+        this.setState(
+            {
+                stepNum: step,
+                xIsNext: ((step % 2) === 0),
+            }
+        )
+    }
     render() {
+        //* 準備區 */
+        const history = this.state.history;
+        const current = history[this.state.stepNum];
+        const winner = calculateWinner(current.squares);
+        // 雖然是 const，但是一有改變，就會重新 render。
+
+        const moves = history.map(
+            (step, move) => {
+                const desc = move ?
+                    'Go to move #' + move :
+                    'Go to game start';
+                return (
+                    <li key={move}>
+                        <button
+                            onClick={
+                                () => this.jumpTo(step)
+                            }
+                        >
+                            {desc}
+                        </button>
+                    </li>
+                )
+            }
+        )
+
+        let status;
+        if (winner) {
+            status = '獲勝者是: ' + winner;
+        } else if (current.squares.indexOf(null) === -1) {
+            status = '平手';
+        } else {
+            status = '現在輪到: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+        //* 輸出區 */
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)}
+                    />
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
+                    <div className="status">{status}</div>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
@@ -115,9 +158,11 @@ function calculateWinner(squares) {
     ];
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-        }
+        if (
+            squares[a] &&
+            squares[a] === squares[b] &&
+            squares[a] === squares[c]
+        ) return squares[a];
     }
     return null;
 }
